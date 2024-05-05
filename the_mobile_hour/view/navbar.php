e5d1daeec87fbdb01ba7468be88f2b773df73c7e
Ethan Powell <powellec@yahoo.com>
2024-05-05 22:52:11 +1000

    Added index and navbar
    - added index.php
    - bootstrap linked
    - created navbar
    - navbar a bit iffy

---
+++ the_mobile_hour/view/navbar.php
@@ -0,0 +1,58 @@
+
+<!-- navbar - __DIR__ include in every page -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <!-- logo -->
+  <a class="navbar-brand" href="index.php">
+    <img src="images/logo.png" width="220" height="50" alt="The Mobile Hour Logo">
+  </a>
+  
+  <!-- hamburger toggler -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target= "#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- navbar links -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="view/products.php">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="view/brands.php">Brands</a>
+      </li>
+    </ul>
+
+    <!-- navbar left margin items -->
+    <ul class="navbar-nav ml-auto">
+
+        <!-- search bar -->
+      <form class="d-flex" role="search">
+        <input class="form-control" type="search" placeholder="Search" aria-label="Search" style= "font-size: 15px">
+        <button class="btn btn-outline-white" type="submit">
+          <!-- search svg - magn glass-->
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+          </svg>
+        </button>
+      </form>
+     
+
+      <!-- myacc -->
+      <li class="nav-item ml-2">
+        <a class="nav-link" href="view/login.php">
+        <!-- myacc svg - person (fill) -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+        </svg> 
+       
+        My Account 
+        </a>
+      </li>
+
+    <ul>
+
+  <div>
+    
+</nav>
