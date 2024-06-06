573e4549c29689334cad2caba0a8f2ed51672198
Ethan Powell <powellec@yahoo.com>
2024-06-06 11:32:51 +1000

    Added empty database
    - exported empty database

--- the_mobile_hour/view/navbar.php
+++ the_mobile_hour/view/navbar.php
@@ -17,10 +17,10 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="view/products.php">Products</a>
+        <a class="nav-link" href="products.php">Products</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="view/brands.php">Brands</a>
+        <a class="nav-link" href="brands.php">Brands</a>
       </li>
     </ul>
 
---
+++ the_mobile_hour/view/products.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Products</title>
+
+  <!-- bootstrap CDN link -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+
+  <header>
+    <!-- navbar -->
+    <?php include 'navbar.php'; ?>
+
+    <!-- page title -->
+
+    <div class="container  p-3 ">
+      <div class="row">
+
+        <div class="col-8">
+          <h1 class="display-5">Mobile Phones</h1>
+        </div>
+
+        <div class="col-9 mt-4">
+          <p>
+            Here at The Mobile Hour every minute counts in finding the perfect phone for you. 
+            Explore our collection of cutting-edge mobile devices designed to keep you connected and empowered.
+          </p>
+          <hr>  
+        </div>    
+      </div>
+    </div>
+
+
+  </header>
+
+  <main>
+        <!-- product line 1 -->
+        <div class="container">
+      <div class="row justify-content-center">
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2 ">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- product line 2 -->
+    <div class="container">
+      <div class="row justify-content-center pt-3">
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$19.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <!-- product -->
+        <div class="col-lg-3 col-md-5 col-sm-5 col-5 pt-2">
+          <div class="card bg-light">
+            <img class="card-img-top" src="images/placeholder.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Product Name</h5>
+            
+              <div class="row d-flex">
+                <div class="col-12">
+                  <p class="pt-2"><strong>$199.99</strong></p>
+                </div>
+                
+                <div class="col-12">
+                  <a href="#" class="btn btn-info">Order Now</a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+  
+
+
+
+
+
+  <!-- footer -->
+  <?php include 'footer.php'; ?>
+  <!-- bootstrap js -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
