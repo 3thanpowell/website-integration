4e41a94ea253e32cde81974f287b89032185b48b
Ethan Powell <powellec@yahoo.com>
2024-06-08 17:21:05 +1000

    Updated layout

--- the_mobile_hour/view/product.php
+++ the_mobile_hour/view/product.php
@@ -23,6 +23,8 @@ if ($product_id > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($product['product_name']); ?> - The Mobile Hour</title>
+    
+    <!-- bootstrap CDN -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
@@ -36,53 +38,138 @@ if ($product_id > 0) {
     <main>
         <div class="container mt-4">
             <div class="row">
-                <div class="col-md-6">
-                    <img src="../<?php echo htmlspecialchars($product['image_url']); ?>" alt="<?php echo htmlspecialchars($product['product_name']); ?>" class="img-fluid">
+                <div class="col-md-6 ">
+                    <img src="../<?php echo htmlspecialchars($product['image_url']); ?>" alt="<?php echo htmlspecialchars($product['product_name']); ?>" class="img-fluid border">
                 </div>
                 <div class="col-md-6">
-                    <h1><?php echo htmlspecialchars($product['product_name']); ?></h1>
-                    <h2>$<?php echo htmlspecialchars($product['price']); ?></h2>
-                    <a href="#" class="btn btn-info btn-lg">Order Now</a>
+
+                    <h1 class="display-4">
+                        <?php echo htmlspecialchars($product['product_name']); ?>
+                    </h1>
+
+                    <h2 class="mt-lg-3">
+                        $<?php echo htmlspecialchars($product['price']); ?>
+                    </h2>
+
+                    <a href="#" class="btn btn-danger btn-lg mt-5">
+                        Order Now
+                    </a>
+
                     <h3 class="mt-4">Key Features</h3>
-                    <p>Model: <?php echo htmlspecialchars($product['product_model']); ?></p>
-                    <p>Manufacturer: <?php echo htmlspecialchars($product['manufacturer']); ?></p>
-                    <p><?php echo htmlspecialchars($product['description']); ?></p>
+                    <p>
+                        <strong>Model: </strong> 
+                        <?php echo htmlspecialchars($product['product_model']); ?>
+                    </p>
+
+                    <p>
+                        <strong>Manufacturer: </strong> 
+                        <?php echo htmlspecialchars($product['manufacturer']); ?>
+                    </p>
+
+                    <p>
+                        <span>
+                            <?php echo htmlspecialchars($product['description']); ?>
+                        </span>
+                    </p>
                 </div>
             </div>
             <div class="row mt-4">
-                <div class="col-md-12">
-                    <h3>Specifications</h3>
-                    <div class="row">
-                        <div class="col-md-6">
+                <div class="col-md-12 mt-5">
+
+                    <h1 class = "mb-5">
+                        Specifications
+                    </h1>
+
+                    <div class="row mt-3">
+                        <div class="col-md-6 border border-dark bg-light rounded p-3">
+
                             <h4>Key Specifications</h4>
-                            <ul>
-                                <li>Battery Capacity: <?php echo htmlspecialchars($product['battery']); ?></li>
-                                <li>Operating System: <?php echo htmlspecialchars($product['OS']); ?></li>
-                                <li>Dimensions: <?php echo htmlspecialchars($product['dimensions']); ?></li>
-                                <li>Weight: <?php echo htmlspecialchars($product['weight']); ?></li>
-                            </ul>
+                            
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Battery Capacity</th>
+                                        <td><?php echo htmlspecialchars($product['battery']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Operating System</th>
+                                        <td><?php echo htmlspecialchars($product['OS']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Dimensions</th>
+                                        <td><?php echo htmlspecialchars($product['dimensions']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Weight</th>
+                                        <td><?php echo htmlspecialchars($product['weight']); ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
                         </div>
-                        <div class="col-md-6">
+
+
+                        <div class="col-md-6 border border-dark bg-light rounded p-3">
+
                             <h4>Display</h4>
-                            <ul>
-                                <li>Display: <?php echo htmlspecialchars($product['screensize']); ?></li>
-                                <li>Screen Resolution: <?php echo htmlspecialchars($product['resolution']); ?></li>
-                            </ul>
+
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Display</th>
+                                        <td><?php echo htmlspecialchars($product['screensize']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Screen Resolution</th>
+                                        <td><?php echo htmlspecialchars($product['resolution']); ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+               
                         </div>
-                        <div class="col-md-6">
+
+
+                        <div class="col-md-6 border border-dark bg-light rounded p-3">
+
                             <h4>Camera</h4>
-                            <ul>
-                                <li>Front Camera: <?php echo htmlspecialchars($product['front_camera']); ?></li>
-                                <li>Rear Camera: <?php echo htmlspecialchars($product['rear_camera']); ?></li>
-                            </ul>
+
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Front Camera</th>
+                                        <td><?php echo htmlspecialchars($product['front_camera']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Rear Camera</th>
+                                        <td><?php echo htmlspecialchars($product['rear_camera']); ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
                         </div>
-                        <div class="col-md-6">
+
+
+                        <div class="col-md-6 border border-dark bg-light rounded p-3">
+
                             <h4>Storage and Power</h4>
-                            <ul>
-                                <li>CPU: <?php echo htmlspecialchars($product['CPU']); ?></li>
-                                <li>RAM: <?php echo htmlspecialchars($product['RAM']); ?></li>
-                                <li>Storage: <?php echo htmlspecialchars($product['storage']); ?></li>
-                            </ul>
+
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">CPU</th>
+                                        <td><?php echo htmlspecialchars($product['CPU']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">RAM</th>
+                                        <td><?php echo htmlspecialchars($product['RAM']); ?></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Storage</th>
+                                        <td><?php echo htmlspecialchars($product['storage']); ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
                         </div>
                     </div>
                 </div>
