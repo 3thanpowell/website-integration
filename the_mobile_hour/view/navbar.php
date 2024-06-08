b454b1f4deb81b3fe8a76f1f66b71b55fed11b6c
Ethan Powell <powellec@yahoo.com>
2024-06-08 23:14:47 +1000

    Added firstname
    + firstname when user logged in

--- the_mobile_hour/view/navbar.php
+++ the_mobile_hour/view/navbar.php
@@ -64,7 +64,7 @@ $userRole = $isLoggedIn ? $_SESSION['user_role'] : '';
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
               </svg> 
        
-              My Account 
+              <?php echo htmlspecialchars($_SESSION['firstname']); ?>
             </a>
 
           <?php elseif ($userRole === 'manager'): ?>
@@ -76,7 +76,7 @@ $userRole = $isLoggedIn ? $_SESSION['user_role'] : '';
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
               </svg> 
        
-              My Account 
+              <?php echo htmlspecialchars($_SESSION['firstname']); ?>
             </a> 
           
           <?php else: ?>
@@ -88,7 +88,7 @@ $userRole = $isLoggedIn ? $_SESSION['user_role'] : '';
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
               </svg> 
        
-              My Account 
+              <?php echo htmlspecialchars($_SESSION['firstname']); ?>  
             </a>
           
           <?php endif; ?>
