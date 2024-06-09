cc3bee47d95053886f92147d15c351000cd3193c
Ethan Powell <powellec@yahoo.com>
2024-06-09 10:01:02 +1000

    Order placement process

---
+++ the_mobile_hour/controller/order_process.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../model/functions.php';
+session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['order_product_id'])) {
+    $customerId = $_SESSION['customer_id'];
+    $productId = $_POST['order_product_id'];
+    $priceSold = $_POST['price'];
+
+    if (placeOrder($customerId, $productId, $priceSold)) {
+        header('Location: order_success.php');
+        exit();
+    } else {
+        header('Location: order_failed.php');
+        exit();
+    }
+}
