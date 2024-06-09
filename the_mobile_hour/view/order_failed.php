dc20dacff9a9e2b707a348913730288ef6e23b9c
Ethan Powell <powellec@yahoo.com>
2024-06-09 12:35:49 +1000

    Order fail page
    + redirect here on order placement failure

---
+++ the_mobile_hour/view/order_failed.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Order Failed</title>
+
+  <!-- bootstrap CDN link -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+</head>
+<body>
+
+  <!-- navbar -->
+  <?php include 'navbar.php'; ?>
+
+
+
+  <main class="container">
+    
+    <h1 class="display-4">Order Failed</h1>
+    <hr>
+
+
+    <div class="row">
+
+      <div class="col-12">
+        <p>There was a problem with your order, please try again or contact support.</p>
+
+        <ul class="list-unstyled">
+          <li><strong>Email: </strong> themobilehour@email.com</li>
+          <li><strong>Phone: </strong> +61 412 345 678</li>
+        </ul>
+
+
+      </div>
+
+      <div class="col-12">
+        <a href="products.php">
+
+         <p> 
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223"/>
+            </svg> 
+            
+            Continue Shopping
+          </p>
+
+        
+        </a>
+      </div>
+
+      
+
+
+    </div>
+  </main>
+  
+
+
+
+
+
+  <!-- footer -->
+  <div class="fixed-bottom">
+    <?php include 'footer.php'; ?>
+  </div>
+
+  <!-- bootstrap js -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html
