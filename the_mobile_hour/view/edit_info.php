2dcb8282a41af932431a047a4e87b461e01a0918
Ethan Powell <powellec@yahoo.com>
2024-06-09 20:15:08 +1000

    Cleanup

--- the_mobile_hour/view/edit_info.php
+++ the_mobile_hour/view/edit_info.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Modify Information</title>
+  <title>Edit Information</title>
 
   <!-- bootstrap CDN link -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -60,70 +60,80 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
   <main>
+    
+    <div class="container">
+      <h1 class="display-4 m-4">Edit Information</h1>
+    </div>
+
     <div class="container mt-5 p-5 border border-secondary rounded">
+    
+      <?php if (isset($successMessage)): ?>
+        <div class="alert alert-success"><?php echo $successMessage; ?></div>
+      <?php elseif (isset($errorMessage)): ?>
+        <div class="alert alert-danger"><?php echo $errorMessage; ?></div>
+      <?php endif; ?>
 
-        <h1>Edit Info</h1>
-        <?php if (isset($successMessage)): ?>
-            <div class="alert alert-success"><?php echo $successMessage; ?></div>
-        <?php elseif (isset($errorMessage)): ?>
-            <div class="alert alert-danger"><?php echo $errorMessage; ?></div>
-        <?php endif; ?>
 
+      <form method="POST" action="edit_info.php">
 
-        <form method="POST" action="edit_info.php">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($_SESSION['user_email']); ?>" required>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="firstname">First Name</label>
-                    <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo htmlspecialchars($_SESSION['firstname']); ?>" required>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="lastname">Last Name</label>
-                    <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo htmlspecialchars($_SESSION['lastname']); ?>" required>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="phone">Phone</label>
-                <input type="tel" class="form-control" id="phone" name="phone" pattern='^(?:\+?61|0)[2-478](?:[ \-]?[0-9]){8}$' title="Please enter a valid Australian phone number" value="<?php echo htmlspecialchars($_SESSION['user_phone']); ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="address">Address</label>
-                <input type="text" class="form-control" id="address" name="address" value="<?php echo htmlspecialchars($_SESSION['user_address']); ?>" required>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="postcode">Postcode</label>
-                    <input type="text" class="form-control" id="postcode" name="postcode" pattern="[0-9]{4}" title="Please enter a valid 4-digit postcode" value="<?php echo htmlspecialchars($_SESSION['postcode']); ?>" required>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="city">City</label>
-                    <input type="text" class="form-control" id="city" name="city" value="<?php echo htmlspecialchars($_SESSION['city']); ?>" required>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="state">State</label>
-                <select id="state" name="state" class="form-control" required>
-                    <option value="New South Wales" <?php echo $_SESSION['state'] == 'New South Wales' ? 'selected' : ''; ?>>New South Wales</option>
-                    <option value="Victoria" <?php echo $_SESSION['state'] == 'Victoria' ? 'selected' : ''; ?>>Victoria</option>
-                    <option value="Queensland" <?php echo $_SESSION['state'] == 'Queensland' ? 'selected' : ''; ?>>Queensland</option>
-                    <option value="South Australia" <?php echo $_SESSION['state'] == 'South Australia' ? 'selected' : ''; ?>>South Australia</option>
-                    <option value="Western Australia" <?php echo $_SESSION['state'] == 'Western Australia' ? 'selected' : ''; ?>>Western Australia</option>
-                    <option value="Tasmania" <?php echo $_SESSION['state'] == 'Tasmania' ? 'selected' : ''; ?>>Tasmania</option>
-                    <option value="Australian Capital Territory" <?php echo $_SESSION['state'] == 'Australian Capital Territory' ? 'selected' : ''; ?>>Australian Capital Territory</option>
-                    <option value="Northern Territory" <?php echo $_SESSION['state'] == 'Northern Territory' ? 'selected' : ''; ?>>Northern Territory</option>
-                </select>
-            </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($_SESSION['user_email']); ?>" required>
+        </div>
 
-            <button type="submit" class="btn btn-dark">Update Info</button>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="firstname">First Name</label>
+            <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo htmlspecialchars($_SESSION['firstname']); ?>" required>
+          </div>
 
-            <button type="reset" class="btn btn-secondary">Cancel Changes</button>
-        </form>
+          <div class="form-group col-md-6">
+            <label for="lastname">Last Name</label>
+            <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo htmlspecialchars($_SESSION['lastname']); ?>" required>
+          </div>  
+        </div>
+
+        <div class="form-group">
+          <label for="phone">Phone</label>
+            <input type="tel" class="form-control" id="phone" name="phone" pattern='^(?:\+?61|0)[2-478](?:[ \-]?[0-9]){8}$' title="Please enter a valid Australian phone number" value="<?php echo htmlspecialchars($_SESSION['user_phone']); ?>" required>
+        </div>
+
+        <div class="form-group">
+          <label for="address">Address</label>
+          <input type="text" class="form-control" id="address" name="address" value="<?php echo htmlspecialchars($_SESSION['user_address']); ?>" required>
+        </div>
+
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="postcode">Postcode</label>
+            <input type="text" class="form-control" id="postcode" name="postcode" pattern="[0-9]{4}" title="Please enter a valid 4-digit postcode" value="<?php echo htmlspecialchars($_SESSION['postcode']); ?>" required>
+          </div>
+
+          <div class="form-group col-md-6">
+            <label for="city">City</label>
+            <input type="text" class="form-control" id="city" name="city" value="<?php echo htmlspecialchars($_SESSION['city']); ?>" required>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="state">State</label>
+          <select id="state" name="state" class="form-control" required>
+            <option value="New South Wales" <?php echo $_SESSION['state'] == 'New South Wales' ? 'selected' : ''; ?>>New South Wales</option>
+            <option value="Victoria" <?php echo $_SESSION['state'] == 'Victoria' ? 'selected' : ''; ?>>Victoria</option>
+            <option value="Queensland" <?php echo $_SESSION['state'] == 'Queensland' ? 'selected' : ''; ?>>Queensland</option>
+            <option value="South Australia" <?php echo $_SESSION['state'] == 'South Australia' ? 'selected' : ''; ?>>South Australia</option>
+            <option value="Western Australia" <?php echo $_SESSION['state'] == 'Western Australia' ? 'selected' : ''; ?>>Western Australia</option>
+            <option value="Tasmania" <?php echo $_SESSION['state'] == 'Tasmania' ? 'selected' : ''; ?>>Tasmania</option>
+            <option value="Australian Capital Territory" <?php echo $_SESSION['state'] == 'Australian Capital Territory' ? 'selected' : ''; ?>>Australian Capital Territory</option>
+            <option value="Northern Territory" <?php echo $_SESSION['state'] == 'Northern Territory' ? 'selected' : ''; ?>>Northern Territory</option>
+          </select>
+        </div>
+
+        <button type="submit" class="btn btn-dark">Update Info</button>
+        <button type="reset" class="btn btn-secondary">Cancel Changes</button>
+      </form>
+
     </div>  
-
- 
 
   </main>
 
