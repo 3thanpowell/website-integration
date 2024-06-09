eb1baf5acfa9e45b1863d12e501336a3e8ff59ca
Ethan Powell <powellec@yahoo.com>
2024-06-09 18:37:33 +1000

    Processes order cancels

---
+++ the_mobile_hour/controller/cancel_order.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// kicks user if not logged in or not a customer
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] != 'customer') {
+    header('Location: login.php');
+    exit();
+}
+
+require_once '../model/functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $orderNumber = $_POST['order_number'];
+    cancelOrder($orderNumber);
+    header('Location: ../view/order_history.php');
+    exit();
+}
