269b638df00b9b60ae7acbd883b73c86f134a548
Ethan Powell <powellec@yahoo.com>
2024-06-10 15:31:25 +1000

    Some clean up

--- the_mobile_hour/index.php
+++ the_mobile_hour/index.php
@@ -20,16 +20,14 @@ $productChunks = array_chunk($products, 4);
 
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Mobile Hour</title>
 
   <!-- bootstrap CDN link -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 
-  <title>The Mobile Hour</title>
-
-
   <!-- custom css -->
-    <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
 
 
 </head>
