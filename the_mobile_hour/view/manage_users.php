533b012421d38bff2ff04d7256f9adb2acaab138
Ethan Powell <powellec@yahoo.com>
2024-06-14 22:17:55 +1000

    Updated placeholder

--- the_mobile_hour/view/manage_users.php
+++ the_mobile_hour/view/manage_users.php
@@ -73,7 +73,7 @@ if ($_SESSION['user_role'] == 'manager') {
     <!-- filter and search Form -->
     <form method="GET" action="manage_users.php" class="mb-4">
       <div class="form-row">
-        
+
         <!-- select role only for managers -->
         <?php if ($_SESSION['user_role'] == 'manager') : ?>
           <div class="form-group col-md-4">
@@ -87,7 +87,7 @@ if ($_SESSION['user_role'] == 'manager') {
         <?php endif; ?>
 
         <div class="form-group col-md-4">
-          <input type="text" id="search" name="search" placeholder="Search..." class="form-control" value="<?php echo htmlspecialchars($search); ?>">
+          <input type="text" id="search" name="search" placeholder="Search Name or Email" class="form-control" value="<?php echo htmlspecialchars($search); ?>">
         </div>
 
         <div class="form-group col-md-4">
@@ -155,9 +155,9 @@ if ($_SESSION['user_role'] == 'manager') {
   <?php include 'footer.php'; ?>
 
   <!-- bootstrap js -->
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-pzjw8f+ua7Kw1TIqDQ+KvnfHR3Ew5ne4hi8tW8SY0hlWl7S2p7jhbZoVNAs4QEl6" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 
 </body>
 
