447cb31b56df46f7f04297310228b481420ad3e4
Ethan Powell <powellec@yahoo.com>
2024-06-14 16:26:13 +1000

    User promotion process

---
+++ the_mobile_hour/controller/promote_user.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+// kicks user if not logged in or not manager
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] != 'manager') {
+  header('Location: ../view/login.php');
+  exit();
+}
+
+require_once '../model/functions.php';
+
+$user_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+if ($user_id == 0) {
+  header('Location: ../view/edit_user.php?status=error&id=' . $user_id);
+  exit();
+}
+
+promoteUserToAdmin($user_id);
+header('Location: ../view/edit_user.php?status=promoted&id=' . $user_id);
+exit();
