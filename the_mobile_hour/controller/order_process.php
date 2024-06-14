a1fd86ba2493fdf3fbaaa440e972efbe6b484313
Ethan Powell <powellec@yahoo.com>
2024-06-14 16:26:04 +1000

    Formatting

--- the_mobile_hour/controller/order_process.php
+++ the_mobile_hour/controller/order_process.php
@@ -1,6 +1,15 @@
 <?php
-require_once '../model/functions.php';
 session_start();
+
+// kicks user if not logged in or not a customer
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] != 'customer') {
+  header('Location: ../view/login.php');
+  exit();
+}
+
+require_once '../model/functions.php';
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['order_product_id'])) {
   $customerId = $_SESSION['customer_id'];
   $productId = $_POST['order_product_id'];
