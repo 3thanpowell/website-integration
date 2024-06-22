6c0dda7e4bd81dd2d9bfdefef2caf1c26d9e4a89
Ethan Powell <powellec@yahoo.com>
2024-06-22 11:56:29 +1000

    Add viewport
    +fix layout
    +correct bootstrap links

--- the_mobile_hour/view/dashboard.php
+++ the_mobile_hour/view/dashboard.php
@@ -22,6 +22,7 @@ $recentOrders = getRecentOrders($customerId);
 
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
 
   <!-- bootstrap CDN link -->
@@ -40,101 +41,100 @@ $recentOrders = getRecentOrders($customerId);
 
 
 
-  <body>
 
-    <main>
+  <main>
 
-      <div class="container-fluid p-5">
-        <div class="row">
+    <div class="container-fluid p-5">
+      <div class="row">
 
-          <!-- sidebar -->
-          <div class="col-md-3 col-lg-2 sidebar border-right">
-            <h3 class="my-4">Welcome,
-              <?php echo ucfirst(htmlspecialchars($firstname)); ?>
-            </h3>
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active h6" href="edit_info.php">
-                  Update Account Information
-                </a>
-                <hr>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link h6" href="order_history.php">
-                  Manage Orders
-                </a>
-                <hr>
-              </li>
+        <!-- sidebar -->
+        <div class="col-md-3 col-sm-12 col-lg-2 sidebar border-right">
+          <h3 class="my-4">Welcome,
+            <?php echo ucfirst(htmlspecialchars($firstname)); ?>
+          </h3>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active h6" href="edit_info.php">
+                Update Account Information
+              </a>
+              <hr>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link h6" href="order_history.php">
+                Manage Orders
+              </a>
+              <hr>
+            </li>
 
-              <li class="nav-item">
-                <a class="nav-link btn-secondary text-center btn-lg" href="../controller/logout.php">
-                  <span class="h4">Logout</span>
-                </a>
-              </li>
+            <li class="nav-item">
+              <a class="nav-link btn-secondary text-center btn-lg" href="../controller/logout.php">
+                <span class="h4">Logout</span>
+              </a>
+            </li>
 
-            </ul>
-          </div>
+          </ul>
+        </div>
 
-          <!-- main content -->
-          <div class="col-md-9 col-lg-10">
-            <h1 class="my-4">Recent Orders</h1>
+        <!-- main content -->
+        <div class="col-md-9 col-sm-12 col-lg-10">
+          <h1 class="my-4">Recent Orders</h1>
 
-            <?php if (count($recentOrders) > 0) : ?>
-              <ul class="list-group">
-                <?php foreach ($recentOrders as $order) : ?>
-                  <li class="list-group-item">
-                    <div class="media">
+          <?php if (count($recentOrders) > 0) : ?>
+            <ul class="list-group">
+              <?php foreach ($recentOrders as $order) : ?>
+                <li class="list-group-item">
+                  <div class="media">
 
-                      <!-- product image -->
-                      <img src="../<?php echo htmlspecialchars($order['image_url']); ?>" class="mr-3 img-fluid" style="max-width: 100px;" alt="Product image">
+                    <!-- product image -->
+                    <img src="../<?php echo htmlspecialchars($order['image_url']); ?>" class="mr-3 img-fluid" style="max-width: 100px;" alt="Product image">
 
-                      <div class="media-body">
+                    <div class="media-body">
 
-                        <h4 class="mb-3 h4">
-                          <?php echo htmlspecialchars($order['product_name']); ?>
-                        </h4>
+                      <h4 class="mb-3 h4">
+                        <?php echo htmlspecialchars($order['product_name']); ?>
+                      </h4>
 
-                        <p><strong>Order Number:</strong>
-                          <?php echo htmlspecialchars($order['order_number']); ?>
-                        </p>
+                      <p><strong>Order Number:</strong>
+                        <?php echo htmlspecialchars($order['order_number']); ?>
+                      </p>
 
-                        <p><strong>Order Date:</strong>
-                          <?php echo htmlspecialchars($order['order_date']); ?>
-                        </p>
+                      <p><strong>Order Date:</strong>
+                        <?php echo htmlspecialchars($order['order_date']); ?>
+                      </p>
 
-                        <p><strong>Status:</strong>
-                          <?php echo htmlspecialchars($order['order_status']); ?>
-                        </p>
+                      <p><strong>Status:</strong>
+                        <?php echo htmlspecialchars($order['order_status']); ?>
+                      </p>
 
-                        <p><strong>Delivery Date:</strong>
-                          <?php echo htmlspecialchars($order['order_delivery_date'] ? $order['order_delivery_date'] : 'Not yet delivered'); ?>
-                        </p>
+                      <p><strong>Delivery Date:</strong>
+                        <?php echo htmlspecialchars($order['order_delivery_date'] ? $order['order_delivery_date'] : 'Not yet delivered'); ?>
+                      </p>
 
-                      </div>
                     </div>
-                  </li>
+                  </div>
+                </li>
 
-                <?php endforeach; ?>
-              </ul>
-            <?php else : ?>
-              <p class="h4">No orders yet..</p>
-            <?php endif; ?>
-          </div>
+              <?php endforeach; ?>
+            </ul>
+          <?php else : ?>
+            <p class="h4">No orders yet..</p>
+          <?php endif; ?>
         </div>
       </div>
+    </div>
 
-    </main>
+  </main>
 
 
 
-    <!-- footer -->
-    <?php include 'footer.php'; ?>
+  <!-- footer -->
+  <?php include 'footer.php'; ?>
 
-    <!-- bootstrap js -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <!-- bootstrap js -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 
-  </body>
+</body>
 
 </html>
